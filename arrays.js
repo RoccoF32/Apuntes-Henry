4378e25790ae9a33244a51cf5fb5596e662a38c1
Roccof32 <marquezf06532@gmail.com>
2023-03-06 23:43:17 -0300

    Apuntes 05.03

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var listaDeCompras =[];
+listaDeCompras [1] = 'Lechuga';
+listaDeCompras[3] = 'Tomates'; 
+
+var verLista = listaDeCompras[1];
+console.log(verLista);
+
+//Un elemento que podemos incluir es el elemento length, el cual ayuda a saber cuantos elementos hay en una lista.
+
+console.log(listaDeCompras.length); 
+//Aqui nos imprime 4 por mas que no haya elementos asignados, hay espacios en uso.
+
+var color = ['Azul', "Verde"];
+color.push("Rojo");
+
+console.log(color);
